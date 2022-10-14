be75a9d59831aa10e6cab7aa81f8840a2a9b80e5
Joeri Abbo <joeriabbo@hotmail.com>
2022-10-14 21:53:49 +0200

    Init spring

---
+++ src/main/java/nl/joeriabbo/spacecenter/SpaceCenterApplication.java
@@ -0,0 +1,13 @@
+package nl.joeriabbo.spacecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/joeriabbo/spacecenter/SpaceCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.joeriabbo.spacecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
